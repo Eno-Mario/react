20dbab87ef2087ee34a644b97306441075131a85
Eno-Mario <141042374+Eno-Mario@users.noreply.github.com>
2023-11-08 16:41:35 +0100

    aggiustato comportamento del bottone Reset

--- src/Login.jsx
+++ src/Login.jsx
@@ -40,15 +40,13 @@ export default function Login({func}) {
 
 
   return (
-    <div>
-        <form onSubmit={handleLogin}>
-            <input name="username" type="text" value={data.username} onChange={handleInput} />
-            <input name="password" type="password" value={data.password} onChange={handleInput} />
-            <label htmlFor="save">Remember me!</label>
-            <input name="save" type="checkbox" checked={data.save} onChange={handleInput} />
-            <button disabled={!data.username || !data.password}>Login</button>
-        </form>
-        <button onClick={handleReset}>Reset</button>
-    </div>
+    <form onSubmit={handleLogin}>
+        <input name="username" type="text" value={data.username} onChange={handleInput} />
+        <input name="password" type="password" value={data.password} onChange={handleInput} />
+        <label htmlFor="save">Remember me!</label>
+        <input name="save" type="checkbox" checked={data.save} onChange={handleInput} />
+        <button disabled={!data.username || !data.password}>Login</button>
+        <button type='button' onClick={handleReset}>Reset</button>
+    </form>
   )
 }
