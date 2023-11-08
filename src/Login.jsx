8c8d3f4a5a7287fb00f14c9acd8c08b63dcd3f42
Eno-Mario <141042374+Eno-Mario@users.noreply.github.com>
2023-11-08 16:22:31 +0100

    soluzione

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { useState } from 'react'
 
-export default function Login({func}) {
-    const [data, setData] = useState({
+function initialiseData() {
+    return {
         username: '',
         password: '',
         save: false
-    })
+    }
+}
+
+export default function Login({func}) {
+    const [data, setData] = useState(initialiseData())
 
     function handleInput (event) {
         const value = event.target.value;
@@ -27,6 +31,10 @@ export default function Login({func}) {
         func(loginData)
     }
 
+    function handleReset() {
+        setData(initialiseData())
+    }
+
   return (
     <div>
         <input name="username" type="text" value={data.username} onChange={handleInput} />
@@ -34,6 +42,7 @@ export default function Login({func}) {
         <label htmlFor="save">Remember me!</label>
         <input name="save" type="checkbox" checked={data.save} onChange={handleInput} />
         <button disabled={!data.username || !data.password} onClick={handleLogin}>Login</button>
+        <button onClick={handleReset}>Reset</button>
     </div>
   )
 }
