97368c0abd55dfc9ba04facd91bf9e3e928da1a8
Eno-Mario <pisellienomario@gmail.com>
2023-11-22 13:36:38 +0100

    soluzione

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-## Exercise 5
+## Exercise 41
 
-Create a Welcome function component that receives a name prop and renders the Welcome, {name}! message within a p tag.
+Create a new branch starting from the first props exercise.
 
-Render this component to you App component, passing it a name prop of your choosing. What happens if no name prop is passed to the Welcome component? Can you set a default value for the name prop?
+Modify App component that wraps a Routes component and add a single Route to the / path that renders the Welcome component, passing it a name prop. Render the App component within a BrowserRouter component.
--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import Hello from "./Hello";
 import Message from "./Message"; /* the import created automatically */
 import Welcome from "./Welcome";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Hello />
-      <Message /> {/* we can render the Message component directly within the App component, if we do it we automatically import the Message component */}
-      <Hello /> {/* yes we can use component Hello more than once! */}
-      <Welcome name="Luigi" /> {/* Render the component Welcome to App component, passing it a name prop of your choosing. */}
-      <Welcome/> {/* if no name prop is passed to the Welcome component the default behavior is: undefined is displayed because the name prop is not provided. */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome name="Luigi" />} />
+        {/* Add other routes here if needed */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
