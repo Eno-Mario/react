4df84bfe599e01caccd602ff001f4c5eca254d83
Eno-Mario <pisellienomario@gmail.com>
2023-11-30 17:48:03 +0100

    Soluzione

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,17 @@
 import React, { StrictMode } from "react";
 import { createRoot, render } from "react-dom/client";
+import { SWRConfig } from "swr";
 import App from "./App";
 
-import  "./style.css"
+import "./style.css";
+
+const fetcher = (url) => fetch(url).then((resp) => resp.json());
 
 const root = createRoot(document.getElementById("root"));
-root.render(<StrictMode><App /></StrictMode>);
+root.render(
+  <StrictMode>
+    <SWRConfig value={{ fetcher }}>
+      <App />
+    </SWRConfig>
+  </StrictMode>
+);
--- src/useGithubUser.jsx
+++ src/useGithubUser.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 
-const fetcher = (url) => fetch(url).then((resp) => resp.json());
-
 export default function useGithubUser({ username }) {
   if (!username) {
     return {
@@ -14,8 +12,7 @@ export default function useGithubUser({ username }) {
   }
 
   const { data, error, mutate } = useSWR(
-    `https://api.github.com/users/${username}`,
-    fetcher
+    `https://api.github.com/users/${username}`
   );
 
   const fetchUserData = () => {
