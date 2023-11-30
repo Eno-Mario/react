e22052353751563a392c7924f55c00395ef23479
Eno-Mario <pisellienomario@gmail.com>
2023-11-30 17:22:13 +0100

    soluzione

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-## Exercise 40
+## Exercise 48
 
-Write the useCurrentLocation custom hook, that uses the browser's navigator.geolocation API to get the current location of the user through the getCurrentPosition builtin method.
-The hook should return the current location as well as a function to get the current location, and the error and loading states.
+Modify the useGithubUser custom hook from the Custom Hooks - 3 exercise to use the useSWR hook to fetch the data of a Github user.
--- src/useGithubUser.jsx
+++ src/useGithubUser.jsx
@@ -1,34 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import useSWR from "swr";
 
-export default function useGithubUser({username}) {
-    const [userData, setUserData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null)
+const fetcher = (url) => fetch(url).then((resp) => resp.json());
 
-    function fetchData() {
-      setLoading(true)
-      setError(null)
-
-      fetch(`https://api.github.com/users/${username}`)
-          .then((data) => {
-            if (data.status !== 200) {
-              setError(new Error)
-              return null
-            } else {
-              return data.json()
-            }
-          })
-          .then((data) => setUserData(data))
-          .catch((err) => setError(err))
-          .finally(() => setLoading(false))
-    }
-
-    useEffect(() => fetchData(), [username])
+export default function useGithubUser({ username }) {
+  const { data, error, mutate } = useSWR(
+    `https://api.github.com/users/${username}`,
+    fetcher
+  );
 
   return {
-    userData,
-    loading,
+    userData: data,
+    loading: !data && !error,
     error,
-    onFetchData: fetchData
-  }
+    onFetchData: mutate,
+  };
 }
